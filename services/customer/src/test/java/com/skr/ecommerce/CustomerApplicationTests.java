d553612b83574585c449370d297f5fe7558fdb0d
Shivkaran Ravidas <s.ravidas01@gmail.com>
2025-09-08 11:31:20 +0530

    Initial commit of e-commerce project

---
+++ services/customer/src/test/java/com/skr/ecommerce/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skr.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
